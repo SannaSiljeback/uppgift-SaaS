22bd31411c5291744b6d6b62140facf00a5c1e6b
Sanna Siljebäck <sannasiljeback@gmail.com>
2024-05-09 13:09:14 +0200

    started require role function

--- html/functions.php
+++ html/functions.php
@@ -19,4 +19,20 @@
         }  
     }
 
+
+
+    //funktion för att se en viss roll, fungerar denna? när ska den användas?
+    function require_role($role) {
+        // Kontrollera om användaren är inloggad och har rätt roll
+        if (is_signed_in() && user_has_role($role)) {
+            // Användaren har rätt roll, inget behov av omdirigering
+            return;
+        } else {
+            // Användaren har inte rätt roll, gör en redirect till /no-access.php
+            header("Location: /no-access.php");
+            exit;
+        }
+    }
+
+
 ?>
